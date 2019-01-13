703d587bbc10ab4f44c6b78eb76e878122fe082e
avanvalk <drewvv@gmail.com>
2019-01-13 14:40:32 -0800

    making request handlers return() the content the wrong way

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ var url = require("url");
 function start(route, handle) {
   function onRequest(request, response) {
     var pathname = url.parse(request.url).pathname;
-    console.log("request for " + pathname + " received.");
+    console.log("Request for " + pathname + " received.");
 
-    route(handle, pathname);
-
+    
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
+    var content = route(handle, pathname)
+    response.write(content);
     response.end();
   }
   http.createServer(onRequest).listen(8888);
